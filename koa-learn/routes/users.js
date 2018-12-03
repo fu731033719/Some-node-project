4933ea5f48998ee54ef761965b473c66cd538942
fuchenwei <fuchenwei@citytogo.com>
2018-12-03 10:07:12 +0800

    路由完善

--- koa-learn/routes/users.js
+++ koa-learn/routes/users.js
@@ -17,15 +17,51 @@ router.post('/addPerson', async function (ctx, next) {
     age: ctx.request.body.age
   })
   let code
+  let msg
   try {
     await person.save()
     code = 0
+    msg = '保存成功'
   }catch (e) {
     code = -1
+    msg = '保存失败'
   }
   ctx.body = {
-    code: code
+    code: code,
+    msg: msg
   }
 })
 
+router.post('/getPerson', async function (ctx, next) {
+  const result = await Person.findOne({name:ctx.request.body.name})
+  const results = await Person.find({name:ctx.request.body.name})
+  ctx.body = {
+    code: 0,
+    result,
+    results,
+  }
+})
+
+router.post('/updatePerson', async function (ctx, next) {
+  const result = await Person.where({
+    name: ctx.request.body.name
+  }).update({
+    age: ctx.request.body.age
+  })
+  ctx.body = {
+    code: 0
+  }
+})
+
+router.post('/removePerson', async function (ctx, next) {
+  const result = await Person.where({
+    name: ctx.request.body.name
+  }).remove()
+  ctx.body = {
+    code: 0
+  }
+})
+
+router.post('/getPersonList', async function(ctx, next) {
+})
 module.exports = router
